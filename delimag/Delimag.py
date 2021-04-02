9b8d5c86deee88daeb5873130df763353f0fafac
lrado1 <lazlorado@gmail.com>
2021-04-02 22:25:30 +0000

    Create initial code for the __init__ method

--- delimag/Delimag.py
+++ delimag/Delimag.py
@@ -12,7 +12,12 @@ class Delimag():
     """
 
     def __init__(self, data, var_vertical, var_horizontal="", var_value=""):
-
+        self.data = data
+        self.var_vertical = var_vertical
+        self.var_horizontal = var_horizontal
+        self.var_value = var_value
+        self._aggregation_type = ''
+        self._aggregation_method = ''
     
     
     
