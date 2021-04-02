e02ae28f6e0312a19303c9284480092ed6445dc7
lrado1 <lazlorado@gmail.com>
2021-04-02 22:22:36 +0000

    Create the Delim class with initial skeleton code

---
+++ delimag/Delimag.py
@@ -0,0 +1,76 @@
+class Delimag():
+    """
+    Delimag is a tool to analyze Pandas DataFrame objects with multiselect records.
+        
+        
+    Attributes:
+
+
+    Methods:
+
+            
+    """
+
+    def __init__(self, data, var_vertical, var_horizontal="", var_value=""):
+
+    
+    
+    
+    def __version__():
+        pass
+    
+    
+    
+    def __repr__(self):
+        pass       
+    
+    
+    
+    def distinct_vertical(self, delim=';', drop_na=True):
+        """
+        Return a list of distinct values from the col_vertical variable, after splitting the delimited records.
+        """
+         
+        
+        
+    def distinct_horizontal(self, delim=';', drop_na=True):
+        """
+        Return a list of distinct values from the col_horizontal variable, after splitting the delimited records.
+        """
+       
+        
+    
+    def aggregate_vertical(self, calc='count', delim=';', drop_na=True):
+        """
+        Aggregate along the col_vertical variable.
+        """
+    
+    
+    
+    
+    def aggregate_horizontal(self, calc='count', delim=';', drop_na=True):
+        """
+        Aggregate along the col_horizontal variable.
+        """
+        
+                
+            
+    def aggregate_cross(self, calc='count', delim_vertical=';', delim_horizontal=';'):
+        """
+        Create a cross-tabulation based on two variables and aggregates a third variables's values based on the cross-groupping.
+        """
+        
+        
+    def condition(self, vertical, horizontal, value):
+        """
+        Filter the result set of the aggregation. 
+        """
+
+        
+        
+    def result(self, sort_vertical, sort_horizontal, proportionize=False):
+        """
+        Return the result set of the aggregation in a Pandas DataFrame object in an organized format (sorted, proportionized).
+        """
+        
+        
