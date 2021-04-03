daa0654f8ecde1b44a6cbaefda0ee10610d47cdd
lrado1 <lazlorado@gmail.com>
2021-04-03 00:03:11 +0000

    Add meta-data to setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'delimag',
+      version = '0.01',
+      description = 'Delimag is a tool to analyze Pandas DataFrame objects with multiselect records.',
+      packages = ['pandas', 'delimag'],
+      author = 'lrado1',
+      zip_safe=False)
