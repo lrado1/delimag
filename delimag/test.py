16962bb2713d86070c71dc0d0dad6f6961b46473
lrado1 <lazlorado@gmail.com>
2021-04-03 21:20:32 +0000

    Update content of test_data.csv

--- delimag/test.py
+++ delimag/test.py
@@ -5,12 +5,35 @@
 # /usr/bin/python -m unittest test
 
 import unittest
+import pandas as pd
 
 from delimag import Delimag
 
 
-# Create data for testing
-
+# Open data for testing
+test_df = pd.read_csv('test_data.csv')
 
 class TestDelimagClass(unittest.TestCase):
-    def
+    def setUp(self):
+        self.delimag = Delimag(test_df, 'Object', 'Color', 'Value')
+    
+    def distinct_vertical()
+    
+    
+    
+    distinct_horizontal
+    
+    
+    aggregate_vertical
+    
+    
+    aggregate_horizontal
+    
+    
+    aggregate_cross
+    
+    
+    return_result
+    
+    
+    
