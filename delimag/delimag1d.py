1f72c0bad642261a980367bd5ba3409f83c6ea5e
lrado1 <lazlorado@gmail.com>
2021-04-04 21:41:29 +0000

    Create .__repr__() method

--- delimag/delimag1d.py
+++ delimag/delimag1d.py
@@ -21,3 +21,16 @@ class Delimag1d():
         self.result = []
         self._aggregation_type = 'None'
         self._aggregation_method = 'None'
+        
+    
+    
+    
+    def __repr__(self):
+        
+        df_name =[x for x in globals() if globals()[x] is self.data][0]
+        
+        return (f'DataFrame name: {df_name} \n'
+                f'Groupping variable: {self.var_vertical} \n'
+                f'Value variable: {self.var_value} \n'
+                f'Aggregation type: {self._aggregation_type} \n'
+                f'Aggregation method: {self._aggregation_method}')
