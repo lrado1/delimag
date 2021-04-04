5c11f2746f994139e809f4d2a8fc3c72246b523d
lrado1 <lazlorado@gmail.com>
2021-04-04 22:27:01 +0000

    Fix typo in .__repr__() method

--- delimag/delimag1d.py
+++ delimag/delimag1d.py
@@ -30,7 +30,7 @@ class Delimag1d():
         df_name =[x for x in globals() if globals()[x] is self.data][0]
         
         return (f'DataFrame name: {df_name} \n'
-                f'Groupping variable: {self.var_vertical} \n'
+                f'Groupping variable: {self.var_group} \n'
                 f'Value variable: {self.var_value} \n'
                 f'Aggregation type: {self._aggregation_type} \n'
                 f'Aggregation method: {self._aggregation_method}')
